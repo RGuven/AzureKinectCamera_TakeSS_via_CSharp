e23b7a4b055a299b3dfd7724e41faeb46f74416a
RGuven <rguven887@gmail.com>
2022-03-30 08:36:32 +0300

    unnecessary configs

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # AzureKinectCamera_TakeSS_via_CSharp
 This Basic App Allows You to Capture and Save Screenshots from Azure Kinect DK Camera
+
+![Screenshot](Images\app_appearance.png)
---
+++ packages/Microsoft.Azure.Kinect.Sensor.1.2.0-beta.0/build/native/include/k4a/k4aversion.h
@@ -0,0 +1,16 @@
+/* Copyright (c) Microsoft Corporation. All rights reserved.
+   Licensed under the MIT License. */
+
+#ifndef K4AVERSION_H
+#define K4AVERSION_H
+
+#define K4A_VERSION_MAJOR          1
+#define K4A_VERSION_MINOR          2
+#define K4A_VERSION_PATCH          0
+#define K4A_VERSION_PRERELEASE     "beta.0"
+#define K4A_VERSION_BUILD_METADATA ""
+
+#define K4A_VERSION_STR            "1.2.0-beta.0"
+
+#endif
+
